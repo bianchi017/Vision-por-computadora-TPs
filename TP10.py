61cdc2281358a9351af349de4477de14dca14046
Nicolás Bianciotti <62455993+bianchi017@users.noreply.github.com>
2020-06-26 19:56:02 -0300

    Fixed TP10

--- TP10.py
+++ TP10.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-imagen = cv2.imread('figuras.png', cv2.IMREAD_COLOR)
+imagen = cv2.imread('monedas-naipes.jpg', cv2.IMREAD_COLOR)
 # Convertir la imagen en escala de grises
 gray = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
 # Convertir la imagen en binaria
@@ -11,7 +11,6 @@ canny = cv2.dilate(canny, None, iterations=1)
 canny = cv2.erode(canny, None, iterations=1)
 # Detectar los contornos de los objetos en la imagen
 cnts, _ = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # en OpenCV 4
-print('Presionar la tecla q o esc para salir y cualquier otra tecla para detectar objetos')
 # c (contorno a analizar), epsilon (precisión de aproximación)
 for c in cnts:
     # Establecer un porcentaje de aproximación
@@ -19,40 +18,32 @@ for c in cnts:
     approx = cv2.approxPolyDP(c, epsilon, True)
     x, y, w, h = cv2.boundingRect(approx)
     # Averiguar la cantidad de vértices detectados del objeto
-    if len(approx) == 3:
-        cv2.putText(imagen, 'Triangulo', (x, y - 5), 1, 1, (0, 255, 0), 1)
-        print('Triángulo')
-
     if len(approx) == 4:
         aspectRatio = float(w) / h
 
         if aspectRatio == 1:
-            cv2.putText(imagen, 'Cuadrado', (x + 7, y - 20), 1, 1, (0, 255, 0), 1)
-            cv2.putText(imagen, "(ancho: " + str(w) + ", alto: " + str(h) + ")", (x - 40, y - 5), 1, 1, (0, 255, 0), 1)
+            cv2.putText(imagen, 'Cuadrado', (x + 20, y - 20), 1, 1, (0, 0, 0), 1)
+            cv2.putText(imagen, "(ancho: " + str(w) + ", alto: " + str(h) + ")", (x - 40, y - 5), 1, 1, (0, 0, 0), 1)
+            cv2.drawContours(imagen, [approx], 0, (0, 0, 0), 2)
             print('Cuadrado: ancho,alto =', w, h, 'píxeles')
         else:
-            cv2.putText(imagen, 'Rectangulo', (x + 8, y - 20), 1, 1, (0, 255, 0), 1)
-            cv2.putText(imagen, "(ancho: " + str(w) + ", alto: " + str(h) + ")", (x - 32, y - 5), 1, 1, (0, 255, 0), 1)
+            cv2.putText(imagen, 'Rectangulo', (x + 70, y - 20), 1, 1, (0, 0, 0), 1)
+            cv2.putText(imagen, "(ancho: " + str(w) + ", alto: " + str(h) + ")", (x + 20, y - 5), 1, 1, (0, 0, 0), 1)
+            cv2.drawContours(imagen, [approx], 0, (0, 0, 0), 2)
             print('Rectángulo: ancho,alto =', w, h, 'píxeles')
-
-    if len(approx) == 5:
-        cv2.putText(imagen, 'Pentagono', (x, y - 5), 1, 1, (0, 255, 0), 1)
-        print('Pentágono')
-
-    if len(approx) == 6:
-        cv2.putText(imagen, 'Hexagono', (x + 5, y - 5), 1, 1, (0, 255, 0), 1)
-        print('Hexágono')
 
     if len(approx) > 10:
         area = cv2.contourArea(c)
         radio = np.sqrt(area / np.pi)
-        cv2.putText(imagen, 'Circulo', (x + 8, y - 20), 1, 1, (0, 255, 0), 1)
-        cv2.putText(imagen, "(radio: " + str(int(round(radio))) + ")", (x - 5, y - 5), 1, 1, (0, 255, 0), 1)
+        cv2.putText(imagen, 'Circulo', (x + 15, y - 20), 1, 1, (0, 0, 0), 1)
+        cv2.putText(imagen, "(radio: " + str(int(round(radio))) + ")", (x - 1, y - 5), 1, 1, (0, 0, 0), 1)
+        cv2.drawContours(imagen, [approx], 0, (0, 0, 0), 2)
         print('Círculo: radio =', int(round(radio)), 'píxeles')
 
-    # Dibujar contornos en los objetos detectados y esperar a presionar una tecla para continuar
-    cv2.drawContours(imagen, [approx], 0, (0, 255, 0), 2)
-    cv2.imshow('Imagen', imagen)
-    key = cv2.waitKey(0)
+# Mostrar el resultado
+while 1:
+    cv2.imshow('Resultado', imagen)
+    key = cv2.waitKey(1)
     if key == ord('q') or key == 27:
         break
+cv2.destroyAllWindows()
